f16f08ec8e59dcb619cf3004e353d4a0783443e7
Saundra Catalina <saundracatalina@gmail.com>
2021-05-08 20:09:51 -0600

    Add passing test solve_for_x

--- enumerables/exercises_1/spec/basic_enums_spec.rb
+++ enumerables/exercises_1/spec/basic_enums_spec.rb
@@ -2,12 +2,12 @@ RSpec.describe 'Basic Enums Test' do
   it 'solve for x' do
     s = ''
     5.times do
-      # write code here
+      s.concat('x')
     end
     expect(s).to eq('xxxxx')
   end
 
-  xit 'simple sum' do
+  it 'simple sum' do
     sum = 0
     numbers = [1, 2, 3, 4, 5]
     # write code here
